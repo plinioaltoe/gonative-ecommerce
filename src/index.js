e889d439bb6aebdd9aebd84014c4243d09b9aa21
plinioaltoe <plinioaltoe@yahoo.com.br>
2019-05-23 13:52:38 -0300

    config do code push

--- src/index.js
+++ src/index.js
@@ -2,6 +2,9 @@ import '~/config/ReactotronConfig'
 import '~/config/DevToolsConfig'
 import React from 'react'
 import { Provider } from 'react-redux'
+
+import CodePush from 'react-native-code-push'
+
 import store from '~/store'
 import Routes from './routes'
 
@@ -11,4 +14,6 @@ const App = () => (
   </Provider>
 )
 
-export default App
+export default CodePush({
+  checkFrequency: CodePush.CheckFrequency.ON_APP_RESUME
+})(App)
